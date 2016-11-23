cbb97b4211ff013c64e62eea0bc6220732dbf9dd
RK900 <rovik05@gmail.com>
2016-11-22 17:53:18 -0800

    Add general flu prediction model

---
+++ Flu-Models/Flu-Tree.py
@@ -0,0 +1,10 @@
+'''
+Author: Rohan Koodli
+A general Flu Prediction algorithm that can be used with H1N1 & H3N2 HA & NA proteins
+'''
+
+from Bio import SeqIO
+import os
+path = os.getcwd()
+print path
+seqs = list(SeqIO.parse(path + 'Flu_Data\H1N1\HA\H1N1-HA-1000.fasta','fasta'))
